bfa2cb5f2c8077fa29cf56c7f87dd564377e43b1
nileshrajpur0hit <nileshpurohit5611@gmail.com>
2025-09-27 13:25:39 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "zomato-clone-with-react"
+  base: "/zomato-clone-with-react/"
 })
  
